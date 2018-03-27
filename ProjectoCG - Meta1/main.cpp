79bdce10546918c3b7b7a682fb488043c5a068da
Jose miguel Mendes Canelha <josemc@student.dei.uc.pt>
2018-03-27 14:39:11 +0100

    Initial Commit

---
+++ ProjectoCG - Meta1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectoCG - Meta1
+//
+//  Created by Ze Canelha on 27/03/18.
+//  Copyright © 2018 Ze Canelha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
